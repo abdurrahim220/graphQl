129ce41861407d2641f3bff4f411062c41c3124c
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-03-16 17:08:13 +0600

    all packages added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+
+const {graphqlHTTP} = require('express-graphql')
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/graphql",graphqlHTTP({
+    
+}))
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
