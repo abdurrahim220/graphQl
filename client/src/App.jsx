6500ddca75f710853a82ff995ac6e184e7ec1861
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-03-17 02:25:12 +0600

    delete client data done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,28 @@ import Header from "./components/Header";
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
@@ -15,7 +34,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
-          <Clients/>
+          <Clients />
         </div>
       </ApolloProvider>
     </>
