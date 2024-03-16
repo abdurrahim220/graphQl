b9399b4390e8f8763057e2f0b1c7a6340ddf9680
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-03-16 19:41:27 +0600

    database connected and fake json data called

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 require("dotenv").config();
-
-const {graphqlHTTP} = require('express-graphql')
-
+const colors = require('colors');
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./server/schema/schema");
+const connectDB = require("./server/config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use("/graphql",graphqlHTTP({
-    
-}))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, () => {
+  connectDB()
   console.log(`server is running on port ${port}`);
 });
