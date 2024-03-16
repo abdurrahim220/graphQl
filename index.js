f5e5f2b008bcd0f77909a4c8d69d9a3b86d64699
abdurrahim220 <z.abdurrahim5@gmail.com>
2024-03-17 02:57:55 +0600

    upload problem

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 require("dotenv").config();
 const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
@@ -7,8 +8,17 @@ const schema = require("./server/schema/schema");
 const connectDB = require("./server/config/db");
 const port = process.env.PORT || 5000;
 
+// const __dirname = path.resolve();
+
 const app = express();
 app.use(cors());
+app.use(express.json()); 
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(
   "/graphql",
